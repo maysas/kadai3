d9ea13d11e934c1c516cf4e5dea213cbee6ddebd
Mayumi SASAKI <a1418ms@aiit.ac.jp>
2015-06-05 01:25:24 +0900

    test3

--- test.bash
+++ test.bash
@@ -1,4 +1,21 @@
 #!/bin/bash
 
-echo "This is a TEST."
-echo "BBBBB"
+tmp=/tmp/$$
+
+ERROR_EXIT () {
+    echo "$1" >&2	
+    rm -f $tmp-*
+    exit 1
+}
+
+#####################################
+# TEST1
+#####################################
+
+echo 20 > $tmp-ans
+./mintest.bash 40 20 > $tmp-out || ERROR_EXIT "TEST1-1"
+diff $tmp-ans $tmp-out || ERROR_EXIT "TEST1-2"
+
+echo OK
+rm -f $tmp-*
+exit 0
