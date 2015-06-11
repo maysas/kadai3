2938c320c8fb96ab62897b81f6d61c7a4a483f42
Mayumi SASAKI <a1418ms@aiit.ac.jp>
2015-06-11 16:22:11 +0900

    test9

--- test.bash
+++ test.bash
@@ -20,9 +20,9 @@ echo 10 > $tmp-ans
 ./mintest.bash 10 50 > $tmp-out || ERROR_EXIT "TEST1-1"
 diff $tmp-ans $tmp-out || ERROR_EXIT "TEST1-2"
 
-#echo a > $tmp-ans
-#./mintest.bash a b > $tmp-out || ERROR_EXIT "TEST1-1"
-#diff $tmp-ans $tmp-out || ERROR_EXIT "TEST1-2"
+echo a > $tmp-ans
+./mintest.bash a b > $tmp-out || ERROR_EXIT "TEST1-1"
+diff $tmp-ans $tmp-out || ERROR_EXIT "TEST1-2"
 
 
 echo OK
