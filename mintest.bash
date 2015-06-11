014202ea46826fe7a5ab007f0c97dedd2490447c
Mayumi SASAKI <a1418ms@aiit.ac.jp>
2015-06-11 16:20:19 +0900

    mintest2

--- mintest.bash
+++ mintest.bash
@@ -1,28 +1,50 @@
-#!/bin/bash
+#!/bin/bash 
 
-v=("$@");
-max=$1;
-min=$2;
-memo=0;
+    expr "$1" + 1 >/dev/null 2>&1
+    if [ $? -ge 2 ]; then
+#        return 1
+        exit 2
+    fi
+    
+    expr "$2" + 1 >/dev/null 2>&1
+    if [ $? -ge 2 ]; then
+#        return 1
+        exit 2
+    fi
 
-if [ $max -lt $min ] ; then
-    memo=$max;
-    max=$min;
-    min=$memo;
-fi
+#    return 0
 
-if [ $min -ne 0 ] ; then
-    a=`expr $max % $min` ;
-else
-    a=0
-fi
+  
+    v=("$@");
+    max=$1;
+    min=$2;
+    memo=0;
 
-while [ $a -ne 0 ]
-do
-    max=$min;
-    min=$a;
-    a=`expr $max % $min`
-done;
+#if   IsNumeric $@  then 
+
+    if [ $max -lt $min ] ; then
+        memo=$max;
+        max=$min;
+        min=$memo;
+    fi
+
+    if [ $min -ne 0 ] ; then
+        a=`expr $max % $min` ;
+    else
+        a=0
+    fi
+
+    while [ $a -ne 0 ]
+    do
+        max=$min;
+        min=$a;
+        a=`expr $max % $min`
+    done;
+
+#else
+
+#    exit 0
+
+#fi
 
 echo $min
-
