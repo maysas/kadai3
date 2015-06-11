3c39fefa69ce116849f68ae1eda42096cdd482f7
Mayumi SASAKI <a1418ms@aiit.ac.jp>
2015-06-11 14:26:07 +0900

    test7

--- test.bash
+++ test.bash
@@ -12,9 +12,18 @@ ERROR_EXIT () {
 # TEST1
 #####################################
 
-echo 49 > $tmp-ans
-./mintest.bash 196 245 > $tmp-out || ERROR_EXIT "TEST1-1"
+echo 50 > $tmp-ans
+./mintest.bash 100 50 > $tmp-out || ERROR_EXIT "TEST1-1"
 diff $tmp-ans $tmp-out || ERROR_EXIT "TEST1-2"
+
+echo 10 > $tmp-ans
+./mintest.bash 10 50 > $tmp-out || ERROR_EXIT "TEST1-1"
+diff $tmp-ans $tmp-out || ERROR_EXIT "TEST1-2"
+
+echo a > $tmp-ans
+./mintest.bash a b > $tmp-out || ERROR_EXIT "TEST1-1"
+diff $tmp-ans $tmp-out || ERROR_EXIT "TEST1-2"
+
 
 echo OK
 rm -f $tmp-*
